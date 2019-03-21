1ec26a02e73263cfa6d3ffc537591835b665c4fd
srunstoppable <841430159@qq.com>
2019-03-21 11:16:18 +0800

    first commit

---
+++ src/main/java/com/example/hjh/HjhApplication.java
@@ -0,0 +1,13 @@
+package com.example.hjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjhApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/hjh/controller/BaseController.java
@@ -0,0 +1,41 @@
+package com.example.hjh.controller;
+
+import com.example.hjh.utils.CheckUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author s r
+ * @date 2019/3/18
+ */
+@RestController
+public class BaseController {
+
+    @GetMapping(value ="wx")
+    public void login(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("success");
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            if(CheckUtil.checkSignature(signature, timestamp, nonce)){
+                out.write(echostr);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            out.close();
+        }
+
+    }
+
+}
